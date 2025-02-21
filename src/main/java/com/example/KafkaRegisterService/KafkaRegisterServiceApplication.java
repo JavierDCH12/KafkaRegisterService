135d692160ff04318a50df8a18de34bb06c9ddb7
JAVIER DELGADO CHACON <6003298@viewnext.com>
2025-02-21 11:43:51 +0100

    first commit

---
+++ src/main/java/com/example/KafkaRegisterService/KafkaRegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.KafkaRegisterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRegisterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KafkaRegisterService/KafkaRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KafkaRegisterService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
