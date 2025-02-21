f7fa2b89164d1d3c68c89873c587e8e861404e99
JAVIER DELGADO CHACON <6003298@viewnext.com>
2025-02-21 12:34:13 +0100

    added CONTROLLER to finish PRODUCER

--- src/main/java/com/example/KafkaRegisterService/controller/UserController.java
+++ src/main/java/com/example/KafkaRegisterService/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.KafkaRegisterService.controller;
 
+import com.example.KafkaRegisterService.model.User;
+import com.example.KafkaRegisterService.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+
+
+    @PostMapping("/register")
+    public User registerUser(@RequestParam String name, @RequestParam String email) {
+        return userService.registerUser(name, email);
+    }
 }
---
+++ src/main/java/com/example/KafkaRegisterService/service/UserEventListener.java
@@ -0,0 +1,4 @@
+package com.example.KafkaRegisterService.service;
+
+public class UserEventListener {
+}
