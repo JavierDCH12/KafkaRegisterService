bd142b37132f4b99747bb2385695adb582289a67
JAVIER DELGADO CHACON <6003298@viewnext.com>
2025-02-21 12:40:53 +0100

    added CONSUMER

--- src/main/java/com/example/KafkaRegisterService/service/UserEventListener.java
+++ src/main/java/com/example/KafkaRegisterService/service/UserEventListener.java
@@ -1,4 +1,19 @@
 package com.example.KafkaRegisterService.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserEventListener {
+
+    @KafkaListener(topics = "user-registration", groupId = "user-consumer-group")
+    public void consumeUserEvent(ConsumerRecord<String, String> record) throws Exception  {
+
+        System.out.println("📥 Evento recibido en Kafka: " + record.value());
+    }
+
+
+
+
 }
